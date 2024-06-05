0d5cc59dbe076903d08a8c8dbf4c8124ddd16baa
Levi-Bloom <levibloom03@gmail.com>
2024-06-05 11:41:49 -0400

    Second commit

--- pages/post.js
+++ pages/post.js
@@ -1,39 +1,43 @@
 "use client";
 import { useEffect, useState, React } from 'react';
 
-const Post = (props) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch('/api/product/${props.id}/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  };
-
-  <div>
-  return (
-   {data.map((r) => {
-    return( 
-      <div key = {r.id}>
-      <p> {r.title} </p>
-      <p> {r.content}</p>
+export default function Post(props) {
+      const [data, setData] = useState(null);
+      const [loading, setLoading] = useState(true);
+    
+      useEffect(() => {
+        fetch('/api/product/${props.id}/posts')
+          .then((response) => response.json())
+          .then((data) => {
+            setData(data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+          });
+      }, []);
+    
+      if (loading) {
+        return <p>Loading...</p>;
+      };
+    
+      <div>
+      return (
+       {data.map((r) => {
+        return( 
+          <div key = {r.id}>
+          <p> {r.title} </p>
+          <p> {r.content}</p>
+          </div>
+          
+        )
+       })}
+      );
       </div>
-      
-    )
-   })}
-  );
-  </div>
-}
-export default Post;
+    }
+    
+
+
+
